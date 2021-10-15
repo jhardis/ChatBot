1c1fdeb2f6875dd2ecc9f2575839d2fb5af8a137
Jonathan E. Hardis <jhardis@me.com>
2021-10-14 22:57:43 -0400

    End of Part 6.  End of assignment.

--- ChatBot/ConversationDataSource.swift
+++ ChatBot/ConversationDataSource.swift
@@ -2,17 +2,18 @@ import Foundation
 class ConversationDataSource {
     
     // Array to store questions and answers
-    var messages = [Message]()
+    var messages = [openingLine]
     
     /// The number of Messages in the conversation
-    var messageCount = 0
+//    var messageCount = 0
+    var messageCount: Int {return messages.count}
     
     /// Add a new question to the conversation
     func add(question: String) {
         let message = Message(date: Date(), text: question, type: .question)
         messages.append(message)
         print("Asked to add question: \(question)")
-        messageCount += 1
+//        messageCount += 1
     }
     
     /// Add a new answer to the conversation
@@ -20,7 +21,7 @@ class ConversationDataSource {
         let message = Message(date: Date(), text: answer, type: .answer)
         messages.append(message)
         print("Asked to add answer: \(answer)")
-        messageCount += 1
+//        messageCount += 1
     }
     
     /// The Message at a specific point in the conversation
--- ChatBot/ConversationDelegate.swift
+++ ChatBot/ConversationDelegate.swift
@@ -3,21 +3,26 @@ struct ConversationDelegate {
     func responseTo(question: String) -> String {
         let lowerQuestion = question.lowercased()
         
-        if lowerQuestion.hasPrefix("hello") {
-            return "Why, hello there!"
-        } else if lowerQuestion == "where are the cookies?" {
+        if lowerQuestion == "where are the cookies?" {
             return "In the cookie jar!"
         } else if lowerQuestion.hasPrefix("where") {
             return "To the North!"
+        } else if lowerQuestion.hasPrefix("hello") {
+            return "Why, hello there"
+        } else if lowerQuestion.hasPrefix("who") {
+            return "Who do you think?"
+        } else if lowerQuestion.hasPrefix("why") {
+            return "Why not?"
+        } else if lowerQuestion.hasPrefix("how") {
+            return "Any way that works."
         } else {
+            
             let defaultNumber = question.count % 3
             
-            if defaultNumber == 0 {
-                return "That really depends"
-            } else if defaultNumber == 1 {
-                return "I think so, yes"
-            } else {
-                return "Ask me again tomorrow"
+            switch defaultNumber {
+            case 1: return "That really depends"
+            case 2: return "Hmph.  Really??"
+            default: return "Ask me again tomorrow"
             }
         }
     }
